eb6ac7d4d17cfbc17c958ff6a0ae7369d3817975
josea <josealejandro_pilavizuete@hotmail.com>
2025-01-24 12:51:16 -0500

    COMMIT-INTERFAZ

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -1,7 +1,16 @@
 package Clases;
 
+import Interfaces.Principal;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("");
+        frame.setContentPane(new Principal().JPanelP);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 350);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
     }
 }
---
+++ src/Interfaces/Principal.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import javax.swing.*;
+
+public class Principal {
+    private JButton iniciarSesionButton;
+    private JButton registrarseButton;
+    private JMenuItem Producto1;
+    public JPanel JPanelP;
+    private JTextField textField1;
+}
